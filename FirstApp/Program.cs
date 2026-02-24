d458e18790fbb5c0697c6499e9e830087a0d444a
ali aborady <aboradyali48@gmail.com>
2026-02-24 05:40:16 +0200

    Question02

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,6 +4,24 @@
     {
         static void Main(string[] args)
         {
+            #region Question 1: Regions
+            /*
+            // ══════════════════════════════════════════════════════════════════════
+            //
+            // Q: What is the purpose of #region and #endregion directives in C#? 
+            //    How do they help in code organization?
+            //
+            // ══════════════════════════════════════════════════════════════════════
+
+            //Nested Region Example
+            Console.WriteLine("\n" + new string('-', 70) + "\n");
+
+            //to specify blocks of code that can be expanded or collapsed in the Visual Studio code editor.
+            */
+            #endregion
+
+           
+
 
         }
     }
